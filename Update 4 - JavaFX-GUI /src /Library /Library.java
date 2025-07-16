14c702371a0e76cd640b6ec8021e8e7624d1e4b0
Aaron Schelfo <schelfoaaron918@gmail.com>
2025-07-15 21:33:19 -0400

    Library.java

--- Update 4 - JavaFX-GUI /src /Library /Library.java
+++ Update 4 - JavaFX-GUI /src /Library /Library.java
@@ -5,5 +5,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Library extends Application {
-  
+
+  @Override
+  public void start(Stage stage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+
+    Scene scene = new Scene(root);
+
+    Stage.setScene(scene);
+    stage.show();
+  }
+
+  /**
+   * ()param args the command line arguments
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
